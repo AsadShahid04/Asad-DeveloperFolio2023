b837b4b09f378d8dfe65f9eca1f15d372c28b986
Asad Shahid <asadblue2015@gmail.com>
2022-06-20 01:20:30 -0700

    Final Version

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,10 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by Asad Shahid")}
+          {emoji("Made with ❤️ by Asad Shahid & DevelopFolio Team")}
+        </p>
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          Credits to 
         </p>
         {/*
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
